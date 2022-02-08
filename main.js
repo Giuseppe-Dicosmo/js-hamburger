9635d1f4b914c8af57dab34ef935ca79e76c5cdf
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-08 18:22:52 +0100

    js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-let hamburger = document.querySelector('.hamburger-menu');
+let hamburger = document.querySelector('.fas.fa-bars');
 console.log(hamburger)
+
+let hamburgerActive = document.querySelector('.hamburger-menu');
+console.log(hamburgerActive)
 
 hamburger.addEventListener ('click',
 function() {
-  alert ('Ciao')
-  document.classList.toggle('hamburger-menu')
+  hamburgerActive.classList.add('active')
+  }
+);
+
+hamburgerActive.addEventListener ('click',
+function() {
+  hamburgerActive.classList.remove('active')
   }
 );
